b56639c6361a1c1069d320d3127a019ab1d9e4e6
jchene <jchene@student.42.fr>
2019-11-06 23:45:18 +0100

    Add files via upload

---
+++ ft_strncmp.c
@@ -0,0 +1,13 @@
+int     ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+    int     i;
+
+    i = 0;
+    while ((size_t)i < n)
+    {
+        if (s1[i] != s2[i])
+            return (s1[i] - s2[i]);
+        i++;
+    }
+    return (0);
+}
